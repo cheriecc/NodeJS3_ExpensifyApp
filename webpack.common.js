99e469d85566b83e3691f576539a5e9e2789091f
cheriecc <chensun_2017@126.com>
2024-04-22 01:37:28 +0100

    update webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -1,27 +1,31 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-            loader: 'babel-loader'
-        }
-    }, {
-        test: /\.s?css$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-    }]
-  },
-  devServer: {
-    static: {
-        directory: path.join(__dirname, 'public')
+    entry: './src/app.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
     },
-    historyApiFallback: true
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader'
+            }
+        }, {
+            test: /\.s?css$/,
+            use: ['style-loader', 'css-loader', 'sass-loader']
+        }]
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public')
+        },
+        historyApiFallback: true,
+    },
+    performance: {
+        maxAssetSize: 100000000000,
+        maxEntrypointSize: 4000000000,
+    }
 }
-};
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const Dotnev = require('dotenv-webpack');
+const { DefinePlugin } = require('webpack');
 
 module.exports = {
     mode: 'development',
@@ -32,5 +33,15 @@ module.exports = {
             path: './.env',
             safe: true
         })
+        // new DefinePlugin({
+        //     'process.env.REACT_APP_FIREBASE_DB_API': JSON.stringify(process.env.REACT_APP_FIREBASE_DB_API),
+        //     'process.env.REACT_APP_FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.REACT_APP_FIREBASE_AUTH_DOMAIN),
+        //     'process.env.REACT_APP_FIREBASE_DB_URL': JSON.stringify(process.env.REACT_APP_FIREBASE_DB_URL),
+        //     'process.env.REACT_APP_FIREBASE_PROJECT_ID': JSON.stringify(process.env.REACT_APP_FIREBASE_PROJECT_ID),
+        //     'process.env.REACT_APP_FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.REACT_APP_FIREBASE_STORAGE_BUCKET),
+        //     'process.env.REACT_APP_FIREBASE_SENDER_ID': JSON.stringify(process.env.REACT_APP_FIREBASE_SENDER_ID),
+        //     'process.env.REACT_APP_FIREBASE_APP_ID': JSON.stringify(process.env.REACT_APP_FIREBASE_APP_ID),
+        //     'process.env.REACT_APP_FIREBASE_MEASUREMENT_ID': JSON.stringify(process.env.REACT_APP_FIREBASE_MEASUREMENT_ID)
+        // })
     ]
 }
